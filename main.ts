0b2e1965f8d2cabe2cdfa307f388c10c99260f7a
tondawill <84586443+tondawill@users.noreply.github.com>
2021-09-27 20:20:20 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 5; index++) {
+    	
+    }
+})
+let strip = neopixel.create(DigitalPin.P15, 2, NeoPixelMode.RGB)
+let left = strip.range(0, 1)
+let right = strip.range(1, 1)
+basic.forever(function () {
+	
+})
